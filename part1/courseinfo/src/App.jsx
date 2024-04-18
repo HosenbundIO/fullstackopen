06ea384e478ff089b82a1727127f9371c4fe14d3
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-04-18 11:46:28 +0200

    complete exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,26 @@
 /* eslint-disable react/prop-types */
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
-      <Header title={course} />
-      <Content
-        part={[part1.name, part2.name, part3.name]}
-        exercises={[part1.exercises, part2.exercises, part3.exercises]}
-      />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -29,7 +28,7 @@ const App = () => {
 const Header = (props) => {
   return (
     <header>
-      <h1>{props.title}</h1>
+      <h1>{props.course}</h1>
     </header>
   );
 };
@@ -37,9 +36,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part[0]} exercise={props.exercises[0]} />
-      <Part part={props.part[1]} exercise={props.exercises[1]} />
-      <Part part={props.part[2]} exercise={props.exercises[2]} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
@@ -48,7 +47,7 @@ const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part} {props.exercise}
+        {props.part.name} {props.part.exercise}
       </p>
     </div>
   );
@@ -57,7 +56,12 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.total} </p>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}{" "}
+      </p>
     </div>
   );
 };
