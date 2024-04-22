be441cad8f4553a4b988e9920c9c45e6e28336b8
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-04-22 12:52:56 +0200

    complete exercise 1.13

--- part1/anecdotes/README.md
+++ part1/anecdotes/README.md
@@ -10,10 +10,18 @@ Expand the following application by adding a button that can be clicked to displ
 
 Find out how to generate random numbers in JavaScript, eg. via a search engine or on Mozilla Developer Network. Remember that you can test generating random numbers e.g. straight in the console of your browser.
 
-## 1.13*: anecdotes step 2
+## 1.13\*: anecdotes step 2
 
 Expand your application so that you can vote for the displayed anecdote.
 
 NB store the votes of each anecdote into an array or object in the component's state. Remember that the correct way of updating state stored in complex data structures like objects and arrays is to make a copy of the state.
 
 Using an array might be the simpler choice in this case. Searching the Internet will provide you with lots of hints on how to create a zero-filled array of the desired length.
+
+## 1.14\*: anecdotes step 3
+
+Now implement the final version of the application that displays the anecdote with the largest number of votes.
+
+If multiple anecdotes are tied for first place it is sufficient to just show one of them.
+
+This was the last exercise for this part of the course and it's time to push your code to GitHub and mark all of your finished exercises to the "my submissions" tab of the submission application.
--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,37 +1,45 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const randomAnecdote = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
+  const [selected, setSelected] = useState(0);
+  const [voted, setVoted] = useState(Array(anecdotes.length).fill(0))
+
+  const handleVotesClick = () => {
+    const copyVotes = [...voted]
+    copyVotes[selected] += 1
+    setVoted(copyVotes)
   }
 
+  const handleNextAnecdoteClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+  
+  console.log(voted)
   return (
     <div>
       {anecdotes[selected]}
-      <Button handleClick={randomAnecdote}/>
+      <p>has {voted[selected]} votes</p>
+      <Button
+        handleClick={handleVotesClick} text="vote" />
+      <Button handleClick={handleNextAnecdoteClick} text="next anecdote" />
     </div>
-  )
-}
+  );
+};
 
 const Button = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleClick}>next anecdote</button>
-    </div>
-  )
-}
+  console.log(props)
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
-export default App
+export default App;
