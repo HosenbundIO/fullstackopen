79cd3098693e2ed8e67aa0337db334beccbcc8e7
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-04-25 17:10:52 +0200

    modify index.js to include expressjs

--- part3/coursematerial/index.js
+++ part3/coursematerial/index.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const express = require("express");
+const app = express()
 
 let notes = [
   {
@@ -18,11 +19,11 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(notes));
-});
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+})
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
