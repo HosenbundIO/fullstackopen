89ddad51ebb9144e03de1204a8880172d8c19251
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-04-29 10:54:39 +0200

    complete exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,21 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.format('myformat', (tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'),
+    '-',
+    tokens['response-time'](req, res),
+    'ms',
+    req.method === 'POST' ? JSON.stringify(req.body) : '',
+  ].join(' ');
+});
+
+app.use(morgan('myformat'));
 
 let persons = [
   {
