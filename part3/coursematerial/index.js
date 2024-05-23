502302e18c6de6a2d0501b47e4d077c66aff8e75
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-05-23 13:02:23 +0200

    update coursematerial/index.js

--- part3/coursematerial/index.js
+++ part3/coursematerial/index.js
@@ -25,18 +25,32 @@ app.get('/api/notes', (request, response) => {
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  console.log('notes', notes);
+// app.delete('/api/notes/:id', (request, response, next) => {
+//   const id = Number(request.params.id);
+//   notes = notes.filter((note) => note.id !== id);
+//   console.log('notes', notes);
 
-  response.status(204).end();
+//   response.status(204).end();
+// });
+
+app.delete('api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 // const generateId = () => {
@@ -44,28 +58,69 @@ app.delete('/api/notes/:id', (request, response) => {
 //   return maxId + 1;
 // };
 
-app.post('api/notes', (request, response) => {
+// app.post('api/notes', (request, response) => {
+//   const body = request.body;
+
+//   if (body.content === undefined) {
+//     return response.status(400).json({ error: 'content missing' });
+//   }
+
+//   const note = new Note({
+//     content: body.content,
+//     important: body.important || false,
+//   });
+
+//   note.save().then((savedNote) => {
+//     response.json(savedNote);
+//   });
+// });
+
+app.post('api/notes', (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' });
+  } else {
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    });
+
+    note.save().then((savedNote) => {
+      response.json(savedNote);
+    });
   }
+});
 
-  const note = new Note({
+app.put('api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
     content: body.content,
-    important: body.important || false,
-  });
+    important: body.important,
+  };
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
+const errorHandler = (error, request, response, next) => {
+  console.log('errorHandler', error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+};
+
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
