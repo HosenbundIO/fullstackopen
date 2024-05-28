cc80c577a0a97274d4fc1be8ef77ed265b4a8109
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-05-28 08:41:02 +0200

    refractor whole index.js into modules --> complete exercise 4.2

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,46 +1,6 @@
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = config.MONGODB_URI;
-console.log('connecting to', mongoUrl);
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+const app = require('./app');
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
