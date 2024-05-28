b6151422fef1046cb81695f28ee27ece84a3e20d
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-05-28 09:42:58 +0200

    complete exercise 4.3

---
+++ part4/blog/tests/dummy.test.js
@@ -0,0 +1,10 @@
+const { test, describe } = require('node:test');
+const assert = require('assert');
+const listHelper = require('../utils/list_helper');
+
+test('dummy returns one', () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+  assert.strictEqual(result, 1);
+});
