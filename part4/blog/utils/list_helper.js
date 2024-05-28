dbbe29dfb5c7fb924e49b40fc1b181e865223184
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-05-28 10:10:52 +0200

    complete exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -9,7 +9,15 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const reducer = (max, blog) => {
+    return max.likes > blog.likes ? max : blog;
+  };
+  return blogs.reduce(reducer, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
