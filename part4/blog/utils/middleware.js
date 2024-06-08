1209128ed1ede74901c1763adfe7121ba4eca41d
HosenbundIO <115995049+HosenbundIO@users.noreply.github.com>
2024-06-08 15:44:19 +0200

    complete exercise 4.20

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -8,6 +8,14 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '');
+  }
+  next();
+};
+
 const unknownEndpoint = (request, response, next) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
@@ -41,4 +49,5 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
